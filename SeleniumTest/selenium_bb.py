c8248aa0198f4056be6ded33690d3b19d6059676
Eric Randall <eric.randalls@gmail.com>
2014-07-09 10:37:00 -0400

    Updated Selenium Test
    I only use this file to test things with the Selenium server. It has no
    functionality with regards to the spider itself but it comes in handy so
    I've kept it around...

--- SeleniumTest/selenium_bb.py
+++ SeleniumTest/selenium_bb.py
@@ -1,3 +1,5 @@
+#Just a test script to test functionality in Selenium. This is added to the spider and serves no real purpose except for testing.
+
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 import time
@@ -6,31 +8,35 @@ driver = webdriver.Firefox()
 driver.get("http://www.boltbus.com")
 #assert "Python" in driver.title
 
-#select the region
-elem = driver.find_element_by_name("ctl00$cphM$forwardRouteUC$lstRegion$textBox")
+elem = self.driver.find_element_by_name("ctl00$cphM$forwardRouteUC$lstRegion$textBox")
 elem.click()
-elem = driver.find_element_by_id("ctl00_cphM_forwardRouteUC_lstRegion_repeater_ctl00_link")
+time.sleep(2)
+elem = self.driver.find_element_by_id("ctl00_cphM_forwardRouteUC_lstRegion_repeater_ctl01_link")
 elem.click()
-time.sleep(1)
+time.sleep(2)
 #select the origin
-elem = driver.find_element_by_id("ctl00_cphM_forwardRouteUC_lstOrigin_textBox")
+elem = self.driver.find_element_by_id("ctl00_cphM_forwardRouteUC_lstOrigin_textBox")
 elem.click()
-elem = driver.find_element_by_id("ctl00_cphM_forwardRouteUC_lstOrigin_repeater_ctl00_link")
+time.sleep(2)
+elem = self.driver.find_element_by_id("ctl00_cphM_forwardRouteUC_lstOrigin_repeater_ctl00_link")
 elem.click()
-time.sleep(1)
+time.sleep(2)
 #select the destination
-elem = driver.find_element_by_id("ctl00_cphM_forwardRouteUC_lstDestination_textBox")
+elem = self.driver.find_element_by_id("ctl00_cphM_forwardRouteUC_lstDestination_textBox")
 elem.click()
-elem = driver.find_element_by_id("ctl00_cphM_forwardRouteUC_lstDestination_repeater_ctl00_link")
+time.sleep(2)
+elem = self.driver.find_element_by_id("ctl00_cphM_forwardRouteUC_lstDestination_repeater_ctl00_link")
 elem.click()
 time.sleep(2)
 #select the date
-elem = driver.find_element_by_name("ctl00$cphM$forwardRouteUC$txtDepartureDate")
-elem.click()
-elem.send_keys("05172014")
-#select and click route header in order to refresh the dates
-elem = driver.find_element_by_id("ctl00_cphM_forwardRouteUC_header")
+elem = self.driver.find_element_by_name("ctl00$cphM$forwardRouteUC$txtDepartureDate")
 elem.click()
 time.sleep(2)
+elem.send_keys("07202014")
+elem.send_keys("\t")
+#select and click route header in order to refresh the dates
+#elem = self.driver.find_element_by_id("ctl00_cphM_forwardRouteUC_header")
+#elem.click()
+time.sleep(4)
 
 #driver.close()
