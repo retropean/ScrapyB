f5f813b0a8d6294a80ecaff78e9660185f2cd422
Eric Randall <eric.randalls@gmail.com>
2014-07-09 14:35:07 -0400

    Scrapy is FUNCTIONAL!
    It works! Needed to extract the data from the WebElement instead of
    simply returning the WebElement. Triumph!

--- ScrapyB/spiders/bb_spider.py
+++ ScrapyB/spiders/bb_spider.py
@@ -7,6 +7,9 @@ from ScrapyB.items import FareItem
 from selenium import selenium
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 
 import time
 
@@ -35,43 +38,62 @@ class BBSpider(CrawlSpider):
 		#select the region
 		elem = self.driver.find_element_by_name("ctl00$cphM$forwardRouteUC$lstRegion$textBox")
 		elem.click()
-		time.sleep(2)
+		time.sleep(7)
+		#elem = WebDriverWait(self.driver, 10).until(
+        #EC.element_to_be_clickable((By.ID, "ctl00_cphM_forwardRouteUC_lstRegion_repeater_ctl01_link"))
+		#)
 		elem = self.driver.find_element_by_id("ctl00_cphM_forwardRouteUC_lstRegion_repeater_ctl01_link")
 		elem.click()
-		time.sleep(2)
+		time.sleep(7)
+		#elem = WebDriverWait(self.driver, 10).until(
+        #EC.element_to_be_clickable((By.ID, "ctl00_cphM_forwardRouteUC_lstOrigin_textBox"))
+		#)
 		#select the origin
 		elem = self.driver.find_element_by_id("ctl00_cphM_forwardRouteUC_lstOrigin_textBox")
 		elem.click()
-		time.sleep(2)
+		time.sleep(7)
+		#elem = WebDriverWait(self.driver, 10).until(
+        #EC.element_to_be_clickable((By.ID, "ctl00_cphM_forwardRouteUC_lstOrigin_repeater_ctl00_link"))
+		#)
 		elem = self.driver.find_element_by_id("ctl00_cphM_forwardRouteUC_lstOrigin_repeater_ctl00_link")
 		elem.click()
-		time.sleep(2)
+		time.sleep(7)
+		#elem = WebDriverWait(self.driver, 10).until(
+        #EC.element_to_be_clickable((By.ID, "ctl00_cphM_forwardRouteUC_lstDestination_textBox"))
+		#)
 		#select the destination
 		elem = self.driver.find_element_by_id("ctl00_cphM_forwardRouteUC_lstDestination_textBox")
 		elem.click()
-		time.sleep(2)
+		time.sleep(7)
+		#elem = WebDriverWait(self.driver, 10).until(
+        #EC.element_to_be_clickable((By.ID, "ctl00_cphM_forwardRouteUC_lstDestination_repeater_ctl00_link"))
+		#)
 		elem = self.driver.find_element_by_id("ctl00_cphM_forwardRouteUC_lstDestination_repeater_ctl00_link")
 		elem.click()
-		time.sleep(2)
+		time.sleep(7)
+		#elem = WebDriverWait(self.driver, 10).until(
+        #EC.element_to_be_clickable((By.ID, "ctl00$cphM$forwardRouteUC$txtDepartureDate"))
+		#)
 		#select the date
 		elem = self.driver.find_element_by_name("ctl00$cphM$forwardRouteUC$txtDepartureDate")
 		elem.click()
-		time.sleep(2)
+		#time.sleep(7)
 		elem.send_keys("07202014")
 		elem.send_keys("\t")
 		#select and click route header in order to refresh the dates
-		#elem = self.driver.find_element_by_id("ctl00_cphM_forwardRouteUC_header")
-		#elem.click()
-		time.sleep(4)
+		elem = self.driver.find_element_by_id("ctl00_cphM_forwardRouteUC_header")
+		elem.click()
+		time.sleep(7)
+
 				
 		items = []
 		sites = self.driver.find_elements_by_xpath('//tr[@class="fareviewrow"]')
-
+#get_attribute("value")
 		for site in sites:
 			item = FareItem()
-			item['fare'] = (site.find_elements_by_xpath(".//td[@class='faresColumn0']"))
-			item['origtime'] = (site.find_elements_by_xpath(".//td[@class='faresColumn1']"))
-			item['desttime'] = (site.find_elements_by_xpath(".//td[@class='faresColumn2']"))
+			item['fare'] = (site.find_element_by_xpath(".//td[@class='faresColumn0']").text)
+			item['origtime'] = (site.find_element_by_xpath(".//td[@class='faresColumn1']").text)
+			item['desttime'] = (site.find_element_by_xpath(".//td[@class='faresColumn2']").text)
 #			item['arrcity'] = map(unicode.strip, site.xpath('.//p[@class="arrive"]/text()[3]').extract())
 #			item['arrlocation'] = map(unicode.strip, site.xpath('.//p[@class="arrive"]/text()[5]').extract())
 #			item['deplocation'] = map(unicode.strip, site.xpath('.//li[@class="two"]/p[1]/text()[5]').extract())
